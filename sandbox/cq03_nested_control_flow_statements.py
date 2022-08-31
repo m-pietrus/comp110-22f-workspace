d1a28c8a793eb6182ff24df36190f175de038388
Matthias Pietrus <pietrus@unc.edu>
2022-08-30 23:40:35 -0400

    cq03_nested_control_flow_statements

---
+++ sandbox/cq03_nested_control_flow_statements.py
@@ -0,0 +1,24 @@
+"""A tracing challenge question."""
+
+'''i: int = 0
+s: str = ""
+
+while i < 4:
+    if i % 2 == 0:
+        s = s + "h"
+    else:
+        if i % 3 == 0:
+            s = s + "!"
+        else:
+            s = s + "e"
+    i = i + 1
+
+print (s)'''
+
+if input("name") == "Taylor":
+  date = 0
+  while date < 1021:
+    print("omg midnight!")
+    date = date + 1
+else:
+  print("byeeee")
