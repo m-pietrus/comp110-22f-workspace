73d09940d59a9fe862e446588bb5ae5b0aed858b
Matthias Pietrus <pietrus@unc.edu>
2022-09-15 17:49:45 -0400

    I added emojis

--- exercises/ex03_wordle.py
+++ exercises/ex03_wordle.py
@@ -2,16 +2,41 @@
 
 __author__= "730361113"
 
-def contains_char(wordle: str, letter: str) -> bool:
+def contains_char(guess: str, letter: str) -> bool:
     """Evaluating the inputed guess and its letters."""
 
     assert len(letter) == 1
     i: int = 0
-    while i < len(wordle):
-        if letter == wordle[i]:
+    while i < len(guess):
+        if letter == guess[i]:
             return True
         else:
-            if i < len(wordle):
+            if i < len(guess):
                 i = i + 1
-        if i >= len(wordle):
-            return False  
+        if i >= len(guess):
+            return False
+
+
+
+def emojified(guess: str, wordle: str) -> str:
+    """This will check contains_char function, and replace 
+    True or False with its corresponding Emoji."""
+
+    output: str = ""
+    idx: int = 0
+    WHITE_BOX: str = "\U00002B1C"
+    GREEN_BOX: str = "\U0001F7E9"
+    YELLOW_BOX: str = "\U0001F7E8"
+    
+    assert len(guess) == len(wordle)
+    
+    while idx < len(wordle):
+        if guess[idx] == wordle[idx]:
+            output = output + GREEN_BOX
+        else:
+            if contains_char(wordle, guess[idx]) == True:
+                output = output + YELLOW_BOX
+            else:
+                output = output + WHITE_BOX             
+        idx = idx + 1
+    return output
