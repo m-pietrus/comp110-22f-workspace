3d76c4dd401652f71420b1fd1b06875986353a32
Matthias Pietrus <pietrus@unc.edu>
2022-09-15 19:14:53 -0400

    I wonder if this is really it

--- exercises/ex03_wordle.py
+++ exercises/ex03_wordle.py
@@ -27,9 +27,9 @@ def emojified(guess: str, wordle: str) -> str:
     WHITE_BOX: str = "\U00002B1C"
     GREEN_BOX: str = "\U0001F7E9"
     YELLOW_BOX: str = "\U0001F7E8"
-    
+
     assert len(guess) == len(wordle)
-    
+
     while idx < len(wordle):
         if guess[idx] == wordle[idx]:
             output = output + GREEN_BOX
@@ -40,3 +40,33 @@ def emojified(guess: str, wordle: str) -> str:
                 output = output + WHITE_BOX             
         idx = idx + 1
     return output
+
+def input_guess(length: int,) -> str:
+    """To ensure that the inputed guess is the same length 
+    as the wordle."""
+
+    guess: str = str(input(f"Enter a {length} charchter word: "))
+    
+    while len(guess) != length: 
+        guess: str = input(f"That wasn't {length} chars! Try again: ")
+    return guess
+
+def main() -> None:
+    """The entrypoint of the program and the main game loop."""
+
+    wordle: str = "codes"
+    turn: int = 1
+    attempt: str = ""
+
+    while turn <= 6 and attempt != wordle:
+        print(f" === Turn {turn}/6 === ")
+        attempt = str = input_guess(len(wordle))
+        print(emojified (attempt, wordle))
+        if attempt == wordle:
+            print(f"You won in {turn}/6 turns!")    
+        turn = turn + 1
+    if turn > 6:
+        print("X/6 - Sorry, try again tomorrow!")
+
+if __name__ == "__main__":
+    main()
