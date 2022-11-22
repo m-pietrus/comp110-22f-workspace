1365aa0fdf4ef7a8042edf7a73d7ddeacb99e1a2
Matthias Pietrus <pietrus@unc.edu>
2022-11-22 18:51:58 -0500

    played

--- exercises/ex09/constants.py
+++ exercises/ex09/constants.py
@@ -1,6 +1,6 @@
 """Constants used through the simulation."""
 
-BOUNDS_WIDTH: int = 400
+BOUNDS_WIDTH: int = 800
 MAX_X: float = BOUNDS_WIDTH / 2
 MIN_X: float = -MAX_X
 VIEW_WIDTH: int = BOUNDS_WIDTH + 20
@@ -10,15 +10,15 @@ MAX_Y: float = BOUNDS_HEIGHT / 2
 MIN_Y: float = -MAX_Y
 VIEW_HEIGHT: int = BOUNDS_HEIGHT + 20
 
-CELL_RADIUS: int = 30
-CELL_COUNT: int = 20
-CELL_SPEED: float = 2.0
+CELL_RADIUS: int = 20
+CELL_COUNT: int = 50
+CELL_SPEED: float = 3.0
 
 IMMUNE: int = -1
 VULNERABLE: int = 0
 INFECTED: int = 1
 
 INITIAL_INFECTED: int = 2
-INITIAL_IMMUNE: int = 1
+INITIAL_IMMUNE: int = 0
 
 RECOVERY_PERIOD: int = 180
